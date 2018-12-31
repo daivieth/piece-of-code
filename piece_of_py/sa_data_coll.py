9a2fd53aeef5490bfb4feb236cad37f4e9441cfa
daivieth <daiviet.huynh@gmail.com>
2018-12-31 07:31:32 +0700

    u

---
+++ piece_of_py/sa_data_coll.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2018-present, Taatu Ltd.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import sys
+import os
+
+pdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.abspath(pdir) )
+from settings import *
+sett = sa_path()
+
+sys.path.append(os.path.abspath( sett.get_path_pwd() ))
+from sa_access import *
+access_obj = sa_db_access()
+
+sys.path.append(os.path.abspath( sett.get_path_core() ))
+from ta_calc_ma import *
+
+db_usr = access_obj.username()
+db_pwd = access_obj.password()
+db_name = access_obj.db_name()
+db_srv = access_obj.db_server()
+
+import pymysql.cursors
+connection = pymysql.connect(host=db_srv,
+                             user=db_usr,
+                             password=db_pwd,
+                             db=db_name,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
